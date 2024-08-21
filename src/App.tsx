0f4b5c31db4fbf164205cd38a36393ffd05a6f9f
M Paula <paulla.ps83@gmail.com>
2024-08-21 12:10:40 -0300

    Componente Home - CSS

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Home from './pages/home/home'
+
+function App() {
+  // Código Typescript
+
+
+  return (
+    // Código TSX => HTML + CSS
+    /*<>
+
+    <h1>A turma JS 05</h1>
+    <h2>React é divertido</h2>
+    <p>Lorem lorem lorem</p>
+    </>*/
+
+    <Home />
+
+  )
+}
+
+export default App
